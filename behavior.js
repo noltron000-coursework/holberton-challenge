6414e74a5afb230b9aa54cccb26ad7cdf61b85be
noltron000 <noltron000@gmail.com>
2018-01-01 10:59:48 -0500

    Last-minute Polishing

--- behavior.js
+++ behavior.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
+	var thumbnailElement = document.getElementById("smart_thumbnail");
+	thumbnailElement.addEventListener("click", function() {
+		var thumbnailElement = document.getElementById("smart_thumbnail");
+		if(thumbnailElement.className == "small") {
+			thumbnailElement.className = "big";
+		} else {
+			thumbnailElement.className = "small";
+		}
+	});
 });
